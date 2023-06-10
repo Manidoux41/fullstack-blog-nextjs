a3e7ce8cba37f2027ec9823a31c8947ba9ffe337
Manfred PARBATIA <superflyman90@gmail.com>
2023-06-10 10:25:57 +0200

    second commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 async function fetchBlog() {
   const res = await fetch("http://localhost:3000/api/blog", {
     next: {
@@ -15,10 +17,27 @@ export default async function Home() {
 
   return (
     <main className="w-full h-full">
-      <div className="md:-2/4 sm:w-3/4 m-auto p-4 my-4 rounded-lg bg-slate-800 drop-shadow-xl">
+      <div className="md:-2/4 sm:w-3/4 m-auto p-4 my-5 rounded-lg bg-slate-800 drop-shadow-xl">
         <h1 className="text-slate-200 text-center text-2xl font-extrabold font-[verdana]">
           My fullstack app with Next JS 13.4
         </h1>
+      </div>
+      {/* Link */}
+      <div className="flex my-5">
+        <Link href={"blog/add"} className="md:w-1/6 sm:w-2/4 text-center rounded-md p-2 m-auto bg-slate-200 font-semibold">
+          Add New Blog 🚀 
+        </Link>
+      </div>
+      {/* Blogs */}
+      <div className="w-full flex flex-col justify-center items-center">
+        {posts.map((post:any) => (
+          <div className="w-3/4 p-4nrounded-md mx-3 my-2 bg-slate-200 flex flex-col justify-center">
+            {/* Title and Action */}
+            <div className="flex items-center my-3">
+              <h2 className="mr-auto font-semibold">{post.title}</h2>
+            </div>
+          </div>
+        ))}
       </div>
     </main>
   )
